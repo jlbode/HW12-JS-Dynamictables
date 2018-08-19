d112253625f8c9c5fe14d07349ad74bbd6855ef3
jlbode <36639874+jlbode@users.noreply.github.com>
2018-08-18 21:45:57 -0700

    Added Level 1 and Level 2 Analysis

---
+++ 01-Automatic-Table-and-Date-Search/js/automatic table and date search.js
@@ -0,0 +1,51 @@
+
+// ### Level 1: Automatic Table and Date Search
+
+// * Create a basic HTML web page.
+// * Using the UFO dataset provided in the form of a JavaScript object, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+//   * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least.
+// * Add an `input` tag to your HTML document and write JavaScript code that will search through the `date/time` column to find rows that match user input.
+
+
+
+
+// Console.log the UFO data from data.js
+console.log(dataSet);
+
+
+// Filter the data
+// Select the submit button
+var submit = d3.select("#submit");
+
+submit.on("click", function() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Get the value property of the input element
+    var inputDate = d3.select("#date-form-input").property("value"); 
+
+    console.log(inputDate);
+
+    // DELETE OLD TABLE
+    
+    // Get a reference to the table body
+    var tbody = d3.select("tbody");
+
+    // Select all row insidy table body and delete them
+    var trows = tbody.selectAll("tr");
+    trows.remove();
+
+    // Filter the data
+    var filteredData = dataSet.filter(ufo => (ufo.datetime === inputDate));
+
+    // INSERT TABLE
+    filteredData.forEach( ufo => {
+        console.log(ufo); 
+        var tr= tbody.append("tr"); 
+        Object.values(ufo).forEach(value => {
+            console.log(value);
+            tr.append("td").text(value);
+        });
+    });
+}); 
